4cbf8ccaf328e3624ad6cc89b24d5167ffe813df
Mahit Mehta <mahitmehta@Mahits-MacBook-Air-450.local>
2022-03-05 16:19:11 -0500

    Initial Commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+          <title>Codele</title>
+          <meta name="description" content="Introducing Wordle For Programmers. Attempt Daily Boolean Statement Guessing Puzzles." />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
