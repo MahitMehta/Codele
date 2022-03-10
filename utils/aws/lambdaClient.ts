5ebd6962d77134d0bf35150812bcd810742f738c
Mahit Mehta <mahitmehta@Mahits-MacBook-Air-604.local>
2022-03-10 01:17:52 -0500

    Updated ENV Vars

--- utils/aws/lambdaClient.ts
+++ utils/aws/lambdaClient.ts
@@ -2,10 +2,10 @@ import { LambdaClient } from "@aws-sdk/client-lambda";
 require("dotenv").config();
 
 const lambdaClient = new LambdaClient({ 
-    region: process.env.AWS_DEFAULT_REGION,
+    region: process.env.CODLE_AWS_DEFAULT_REGION,
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+        accessKeyId: process.env.CODLE_AWS_ACCESS_KEY_ID as string,
+        secretAccessKey: process.env.CODLE_AWS_SECRET_ACCESS_KEY as string,
     }
 });
 
