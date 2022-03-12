f129cdf1bb5c3913324f257cbf3d71324cfec155
Mahit Mehta <mahitmehta@Mahits-MacBook-Air-604.local>
2022-03-11 21:59:54 -0500

    Updated Puzzle Query Logic

--- pages/api/puzzle/index.ts
+++ pages/api/puzzle/index.ts
@@ -2,12 +2,10 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import db from "../../../utils/db";
 import moment from "moment-timezone";
 import { firestore } from "firebase-admin";
-import lambdaClient from "../../../utils/aws/lambdaClient";
-import { InvokeCommand } from "@aws-sdk/client-lambda"; 
 
 type Data = {
     sequence: string[],
-    timestamp?: string; 
+    timestamp?: string | null; 
 }
 
 interface IPuzzle extends firestore.DocumentData {
@@ -25,26 +23,16 @@ export async function getPuzzleHandler() : Promise<Data> {
 
     const { docs, size } = await puzzles.orderBy("timestamp").startAt(startDate).endBefore(endDate).limit(1).get();
     
-    if (size) {
+    if (!size) {
         const { sequence, timestamp } : IPuzzle = docs[0].data() as IPuzzle; 
         return { sequence, timestamp: timestamp.toDate().toUTCString() };
     } else {
-        const generateCodleCommand = new InvokeCommand({
-            FunctionName: "GenerateCodle",
-        });
-    
-        const { Payload:newCodlePayload } = await lambdaClient.send(generateCodleCommand);
-        const newCodle =  JSON.parse(Buffer.from(newCodlePayload as any).toString("utf-8"));
-
-        const startOfDay = moment.tz(DEFAULT_TIMEZONE).startOf("day"); // seconds
-        const startOfDayUnixTS = startOfDay.unix();
-
-        puzzles.add({ 
-            sequence: newCodle.puzzle,
-            timestamp: new firestore.Timestamp(startOfDayUnixTS, 0),
-        })
-
-        return { sequence: newCodle.puzzle, timestamp: startOfDay.toDate().toUTCString() };
+        const { docs, size } = await puzzles.orderBy("timestamp", "desc").limit(1).get();
+        if (!size) {
+            return { sequence: [], timestamp: null };
+        }
+        const { sequence, timestamp } : IPuzzle = docs[0].data() as IPuzzle; 
+        return { sequence, timestamp: timestamp.toDate().toUTCString() };
     }
 }
 
@@ -53,5 +41,5 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
     const response = await getPuzzleHandler();
-    res.status(200).json(response || { sequence: [] });
+    res.status(200).json(response || { sequence: [], timestamp: null });
 }
